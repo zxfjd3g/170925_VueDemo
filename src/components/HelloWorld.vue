357843cff52235725ce52e1ae28e17f02f2a36d3
zxfjd3g <258147149@qq.com>
2018-02-26 11:22:01 +0800

    init vue demo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'Vue真好学!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  h2 {
+    color: red;
+  }
+</style>
